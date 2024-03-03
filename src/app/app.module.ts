30c11413e8865dbb60d4a0a58cf8b9410c4dce03
Gabriel Ang <ang.boomer@gmail.com>
2024-03-03 11:42:14 -0600

    initial app generation plus base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { TaskComponent } from './task/task.component';
+import { FilterComponent } from './filter/filter.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    TaskComponent,
+    FilterComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
